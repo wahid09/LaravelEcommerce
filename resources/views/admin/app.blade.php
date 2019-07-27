251a84fffb3940adbe394f97ab320b5a02e129d1
wahid09 <wahid.ullah.cse@gmail.com>
2019-07-27 21:46:41 +0600

    Admin login view is created

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="backend/css/main.css" />
-    <link rel="stylesheet" type="text/css" href="backend/css/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('backend/css/main.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css')}}"/>
 </head>
 <body class="app sidebar-mini rtl">
     @include('admin.partials.header')
@@ -14,10 +14,10 @@
     <main class="app-content">
         @yield('content')
     </main>
-    <script src="backend/js/jquery-3.2.1.min.js"></script>
-    <script src="backend/js/popper.min.js"></script>
-    <script src="backend/js/bootstrap.min.js"></script>
-    <script src="backend/js/main.js"></script>
-    <script src="backend/js/plugins/pace.min.js"></script>
+    <script src="{{asset('backend/js/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{asset('backend/js/popper.min.js')}}"></script>
+    <script src="{{asset('backend/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('backend/js/main.js')}}"></script>
+    <script src="{{asset('backend/js/plugins/pace.min.js')}}"></script>
 </body>
 </html>
