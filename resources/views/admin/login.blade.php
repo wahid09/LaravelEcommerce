fcd641d8984e06d4c2e921046ed2837cbe85cd0f
wahid09 <wahid.ullah.cse@gmail.com>
2019-07-28 01:56:48 +0600

    Login blades edited

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -19,15 +19,26 @@
         <h1>Vali</h1>
       </div>
       <div class="login-box">
-        <form class="login-form" action="https://pratikborsadiya.in/vali-admin/index.html">
+        <form class="login-form" action="{{ route('login') }}" method="POST">
+          @csrf
           <h3 class="login-head"><i class="fa fa-lg fa-fw fa-user"></i>SIGN IN</h3>
           <div class="form-group">
-            <label class="control-label">USERNAME</label>
-            <input class="form-control" type="text" placeholder="Email" autofocus>
+            <label class="control-label">{{ __('E-Mail Address') }}</label>
+            <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" type="email" name="email" value="{{ old('email') }}" required autofocus>
+            @if ($errors->has('email'))
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
           </div>
           <div class="form-group">
-            <label class="control-label">PASSWORD</label>
-            <input class="form-control" type="password" placeholder="Password">
+            <label class="control-label">{{ __('Password') }}</label>
+            <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+            @if ($errors->has('password'))
+              <span class="invalid-feedback" role="alert">
+               <strong>{{ $errors->first('password') }}</strong>
+              </span>
+            @endif
           </div>
           <div class="form-group">
             <div class="utility">
@@ -40,20 +51,7 @@
             </div>
           </div>
           <div class="form-group btn-container">
-            <button class="btn btn-primary btn-block"><i class="fa fa-sign-in fa-lg fa-fw"></i>SIGN IN</button>
-          </div>
-        </form>
-        <form class="forget-form" action="https://pratikborsadiya.in/vali-admin/index.html">
-          <h3 class="login-head"><i class="fa fa-lg fa-fw fa-lock"></i>Forgot Password ?</h3>
-          <div class="form-group">
-            <label class="control-label">EMAIL</label>
-            <input class="form-control" type="text" placeholder="Email">
-          </div>
-          <div class="form-group btn-container">
-            <button class="btn btn-primary btn-block"><i class="fa fa-unlock fa-lg fa-fw"></i>RESET</button>
-          </div>
-          <div class="form-group mt-3">
-            <p class="semibold-text mb-0"><a href="#" data-toggle="flip"><i class="fa fa-angle-left fa-fw"></i> Back to Login</a></p>
+            <input class="btn btn-primary btn-block" type="submit">SIGN IN
           </div>
         </form>
       </div>
