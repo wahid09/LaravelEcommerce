e4ef7c16d459551ec649593da253c6200f6ed475
wahid09 <wahid.ullah.cse@gmail.com>
2019-07-28 00:06:47 +0600

    heroku link check

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="app sidebar-mini rtl">
     @include('admin.partials.header')
