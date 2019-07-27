aff9f1fa6bcd4ee3d4b7c27ed47112209074e724
wahid09 <wahid.ullah.cse@gmail.com>
2019-07-28 00:30:33 +0600

    heroku

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/main.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('backend/css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome/4.7.0/css/font-awesome.min.css') }}"/>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
