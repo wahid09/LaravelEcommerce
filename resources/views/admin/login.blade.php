fb838b82a55585d121732f13411368c01f7291a2
wahid09 <wahid.ullah.cse@gmail.com>
2019-07-28 00:58:22 +0600

    Login blade create

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Main CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('backend/css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{secure_asset('css/main.css')}}">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="../../maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>@yield('title') - {{ config('app.name') }}</title>
@@ -59,11 +59,11 @@
       </div>
     </section>
     <!-- Essential javascripts for application to work-->
-    <script src="{{asset('backend/js/jquery-3.2.1.min.js')}}"></script>
-    <script src="{{asset('backend/js/popper.min.js')}}"></script>
-    <script src="{{asset('backend/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('backend/js/main.js')}}"></script>
+    <script src="{{secure_asset('js/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{secure_asset('js/popper.min.js')}}"></script>
+    <script src="{{secure_asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{secure_asset('js/main.js')}}"></script>
     <!-- The javascript plugin to display page loading on top-->
-    <script src="{{asset('backend/js/plugins/pace.min.js')}}"></script>
+    <script src="{{secure_asset('js/pace.min.js')}}"></script>
   </body>
 </html>
