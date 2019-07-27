da1fc6bac1a92987480f9d103d2319963a9c8fe7
wahid09 <wahid.ullah.cse@gmail.com>
2019-07-28 00:42:13 +0600

    create secure link for css and js

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/main.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/font-awesome.min.css') }}"/>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -16,10 +16,10 @@
     <main class="app-content">
         @yield('content')
     </main>
-    <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
-    <script src="{{ asset('js/popper.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
-    <script src="{{ asset('js/pace.min.js') }}"></script>
+    <script src="{{ secure_asset('js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ secure_asset('js/popper.min.js') }}"></script>
+    <script src="{{ secure_asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('js/main.js') }}"></script>
+    <script src="{{ secure_asset('js/pace.min.js') }}"></script>
 </body>
 </html>
